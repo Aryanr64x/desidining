cc20a63a57f49a4d6f1bcdda899bab84cff53e77
saket <aryansaket@yahoo.com>
2022-05-19 03:03:14 +0530

    all done

--- components/LatestWork.jsx
+++ components/LatestWork.jsx
@@ -28,7 +28,7 @@ const LatestWork = () => {
                             </div>
                             <div className="text-md xl:text-lg  p-4 xl:p-8 text-center lg:text-left">
                               This is a concept  website recently built for  a chinese restaurant at the heart of city
-                              of Bengaluru . The website's intent is to help increase  the restaurant's sales by its conversion centric design 
+                              of Bengaluru . The website&aposs intent is to help increase  the restaurant&aposs sales by its conversion centric design 
                             </div>
                         </Fade>
 
@@ -36,28 +36,7 @@ const LatestWork = () => {
                     <div className="order-3 h-20 lg:hidden">
 
                     </div>
-                    {/* <div className="flex flex-col justify-center order-5 lg:order-4">
-                        <Fade left>
-
-                            <div className="text-center underline text-2xl cursor-pointer">
-                                delhidurbaar.com
-                            </div>
-                            <div className="text-md xl:text-lg  p-4 xl:p-8 text-center lg:text-left">
-                                A Concept website for a team collaboration app , The website is enriched with up to date
-                                professional practices and is not just a static site as it has blog feature too!
-                            </div>
-                        </Fade>
-
-                    </div>
-                    <div className="p-8 order-4 lg:order-5">
-                        <Fade right>
-                            <Tilt>
-                                <img className="h-full rounded-md hover:shadow-lg hover:shadow-black transition-all duration-300 cursor-pointer"
-                                    src="https://i.ibb.co/yV3nwY1/Capture.png" alt="" />
-                            </Tilt>
-                        </Fade>
-
-                    </div> */}
+                  
                 </div>
             </section>
         </div>
