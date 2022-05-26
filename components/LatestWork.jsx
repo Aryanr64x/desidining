28de6892ac27fc02bc3cc16b30fdc9d9e08f9a95
saket <aryansaket@yahoo.com>
2022-05-26 20:18:36 +0530

    extended portfolio

--- components/LatestWork.jsx
+++ components/LatestWork.jsx
@@ -7,7 +7,7 @@ const LatestWork = () => {
         <div>
             <section className="mt-48 px-4 sm:px-20 xl:px-36">
                 <div className="text-3xl sm:text-5xl text-center font-righteous">
-                    Latest Work
+                    Latest Works
                 </div>
                 <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-y-4">
                     <div className="p-8 order-1">
@@ -15,10 +15,10 @@ const LatestWork = () => {
                         <Fade left>
                             <Tilt>
                                 <a href="https://glistening-dasik-8e85c0.netlify.app/">
-                                <img  className="h-full rounded-md hover:shadow-lg hover:shadow-black transition-all duration-300 cursor-pointer" src="https://i.ibb.co/0p7RvYC/bmandar.png" alt="bmandar" border="0" />
+                                    <img className="h-full rounded-md hover:shadow-lg hover:shadow-black transition-all duration-300 cursor-pointer" src="https://i.ibb.co/0p7RvYC/bmandar.png" alt="bmandar" border="0" />
 
                                 </a>
-                             
+
                             </Tilt>
 
                         </Fade>
@@ -27,11 +27,11 @@ const LatestWork = () => {
 
                         <Fade right>
                             <div className="text-center underline text-2xl cursor-pointer">
-                               <a href="https://glistening-dasik-8e85c0.netlify.app/"> bangloremandarin.com </a>
+                                <a href="https://glistening-dasik-8e85c0.netlify.app/"> bangloremandarin.com </a>
                             </div>
                             <div className="text-md xl:text-lg  p-4 xl:p-8 text-center lg:text-left">
-                              This is a concept  website recently built for  a chinese restaurant at the heart of city
-                              of Bengaluru . The website&aposs intent is to help increase  the restaurant&aposs sales by its conversion centric design 
+                                This is a concept  website recently built for  a chinese restaurant at the heart of city
+                                of Bengaluru . The website&aposs intent is to help increase  the restaurant&aposs sales by its conversion centric design
                             </div>
                         </Fade>
 
@@ -39,7 +39,34 @@ const LatestWork = () => {
                     <div className="order-3 h-20 lg:hidden">
 
                     </div>
-                  
+                    <div className="flex flex-col justify-center order-4 lg:order-3">
+
+                        <Fade left>
+                            <div className="text-center underline text-2xl cursor-pointer">
+                                <a href="https://glistening-dasik-8e85c0.netlify.app/"> indiangrillroom.com </a>
+                            </div>
+                            <div className="text-md xl:text-lg  p-4 xl:p-8 text-center lg:text-left">
+                                 This is a concept website for a north indian restaurant named IndianGrillRoom at the heart of 
+                                 Gurugram India.  The website follows all the latest best practices that make it conversion 
+                                 centric
+                            </div>
+                        </Fade>
+
+                    </div>
+                    <div className="p-8 order-5 lg:order-4">
+
+                        <Fade right>
+                            <Tilt>
+                                <a href="https://glistening-dasik-8e85c0.netlify.app/">
+                                    <img className="h-full rounded-md hover:shadow-lg hover:shadow-black transition-all duration-300 cursor-pointer" src="https://i.postimg.cc/65DfBtsF/irgrill.png" alt="bmandar" border="0" />
+
+                                </a>
+
+                            </Tilt>
+
+                        </Fade>
+                    </div>
+
                 </div>
             </section>
         </div>
