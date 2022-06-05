bd4646715f087d8b702f8d2efcdf0a9aa7d09906
saket <aryansaket@yahoo.com>
2022-06-05 17:14:25 +0530

    integrated aos

--- components/LatestWork.jsx
+++ components/LatestWork.jsx
@@ -1,9 +1,16 @@
 import Tilt from 'react-parallax-tilt';
 import Fade from 'react-reveal/Fade'
-
-// import { Fade } from "react-awesome-reveal";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const LatestWork = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+
     return (
         <div className="section container">
             <div className="text-center title display-3">
@@ -13,27 +20,28 @@ const LatestWork = () => {
                 <div className="row mt-5">
                     <div className="col-lg-6 p-lg-5">
                         <a href="https://glistening-dasik-8e85c0.netlify.app/">
-                            <Fade left>
+                         
+                            <div  data-aos="fade-right">
                                 <Tilt>
                                     <img className="img-fluid rounded-3" src="https://i.ibb.co/0p7RvYC/bmandar.png" alt="bmandar"
                                         border="0" />
                                 </Tilt>
-                            </Fade>
+                            </div>
 
                         </a>
                     </div>
                     <div className="col-lg-6 p-lg-5 mt-2 mt-lg-0 d-flex justify-content-center flex-column">
                         <div className="text-center h3">
-                            <Fade right>
+                            <div  data-aos="fade-left">
                                 <a href="https://glistening-dasik-8e85c0.netlify.app/"> bangloremandarin.com </a>
-                            </Fade>
+                            </div>
                         </div>
                         <div className="text-center   mt-2 h6 ">
-                            <Fade right>
+                            <div data-aos="fade-left">
                                 This is a concept website recently built for a chinese restaurant at the heart of city
                                 of Bengaluru . The website&aposs intent is to help increase the restaurant&aposs sales by its
                                 conversion centric design
-                            </Fade>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -41,25 +49,25 @@ const LatestWork = () => {
                     <div
                         className="col-lg-6  order-lg-1  order-2 p-lg-5  mt-2 mt-lg-0 d-flex justify-content-center flex-column">
                         <div className="text-center h3">
-                            <Fade left>
+                            <div data-aos="fade-right">
                                 <a href="https://verdant-haupia-6d06b9.netlify.app"> indiangrillroom.com </a>
-                            </Fade>
+                            </div>
                         </div>
                         <div className="text-center   mt-2 h6 ">
-                            <Fade left>
+                            <div data-aos="fade-right">
                                 This is a concept website for a north indian restaurant named IndianGrillRoom at the heart of
                                 Gurugram India. The website follows all the latest best practices that make it conversion
                                 centric
-                            </Fade>
+                            </div>
                         </div>
                     </div>
                     <div className="col-lg-6 order-lg-2 order-1 p-lg-5">
                         <a href="https://verdant-haupia-6d06b9.netlify.app">
                             <Tilt>
-                                <Fade right>
+                                <div data-aos="fade-left">
                                     <img className="img-fluid rounded-3" src="https://i.postimg.cc/65DfBtsF/irgrill.png" alt="bmandar"
                                         border="0" />
-                                </Fade>
+                                </div>
                             </Tilt>
 
                         </a>
--- components/Tools.jsx
+++ components/Tools.jsx
@@ -1,21 +1,27 @@
-import { Fade } from 'react-reveal';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const Tools = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <div className="container section text-center ">
-       <Fade top>
+       <div data-aos="fade-down">
        <div className="title display-4">
             Only Custom And Quality Code <br />
             No Website Builders!
         </div>
-       </Fade>
-       <Fade bottom>
+       </div>
+       <div data-aos="fade-up">
        <div className="text-center h5 mt-2">
             Cannot stretch more on the limitations that website builders provide, so to deliver quality product I custom
             code everything in html , css and javascript
 
         </div>
-       </Fade>
+       </div>
     </div>
     );
 }
